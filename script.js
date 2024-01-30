e399fa1cde1694502be79959c61c8d7eae0527e1
Andrew <andrewrgpires@gmail.com>
2024-01-30 12:34:20 -0300

    scroll-links

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ const navLink = document.querySelectorAll('.nav__link')
       navMenu.classList.remove('show-menu')
   }
  navLink.forEach(n => n.addEventListener('click', linkAction))
+
+ /* Scroll seções ativos de links */
+ const sections = document.querySelectorAll('section[id]')
+
+  function scrollActive(){
+    const scrollY = window.pageYOffset
+
+     sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+         sectionId = current.getAttribute('id')
+
+          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+          } else {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove('active-link')
+          }
+     })
+  }
+window.addEventListener('scroll', scrollActive)
